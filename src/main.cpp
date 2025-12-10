5e619e2cf99c1939aa2f2f671c4c5315c5b0dd1e
Andrew Potter <agpotter@gmail.com>
2025-12-10 12:17:13 -0800

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,38 @@
+/*
+    monday-night-net: An amateur radio net monitoring utility in gtk4
+    Copyright (C) 2025  Andrew Potter
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+
+#include <peel/Gtk/Gtk.h>
+#include <peel/GLib/MainContext.h>
+
+using namespace peel;
+
+int
+main ()
+{
+  Gtk::init ();
+
+  Gtk::Window *window = Gtk::Window::create ();
+  window->set_title ("Hello, world!");
+  window->present ();
+
+  // Spin the event loop.
+  GLib::MainContext *context = GLib::MainContext::default_ ();
+  while (true)
+    context->iteration (true);
+}
